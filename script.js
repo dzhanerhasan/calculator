b628eb118c3eacb7c5212d155e4b6ae69fa44cce
Dzhaner Hasan <dzhanerrhasan@gmail.com>
2022-01-14 13:01:12 +0200

    fixed the JS calculations

--- script.js
+++ script.js
@@ -13,15 +13,61 @@ function operate(operator, firstNum, secondNum) {
     }
 }
 
-const operatorArr = ["+", "-", "/", "*"]
+let display = document.querySelector('.display');
+const digits = document.querySelectorAll('.digit');
+const operator = document.querySelectorAll('.operator');
+const equals = document.querySelector('.equals');
+const clear = document.querySelector('.clear');
 
-const buttons = document.querySelectorAll('button');
-buttons.forEach((button) => {
-    button.addEventListener('click', function(event) {
-        let display = document.querySelector('.display');
-        if (!button.textContent in operatorArr) {
-        let equation = document.createElement('div');
-        equation = equation.textContent.concat(button.textContent);
+let firstNum = '';
+let secondNum = '';
+let initOperator = '';
+let conseqOperator = '';
+
+digits.forEach((button) => {
+    button.addEventListener('click', function(event) {        
+        if (!initOperator) {
+            firstNum += button.textContent;
+            display.textContent = firstNum;
+        }
+        else {
+            secondNum += button.textContent;
+            display.textContent = secondNum;
         }
     });
+});
+
+operator.forEach((button) => {
+    button.addEventListener('click', function(event) {
+        if (!initOperator) {
+            initOperator = button.textContent;
+        }
+        else {
+            conseqOperator = button.textContent;
+            let result = operate(initOperator,
+                parseFloat(firstNum),
+                parseFloat(secondNum));
+            display.textContent = result;
+            firstNum = result;
+            secondNum = '';
+            initOperator = conseqOperator;
+        }
+    });
+});
+
+equals.addEventListener('click', function() {
+    let result = operate(initOperator,
+        parseFloat(firstNum),
+        parseFloat(secondNum));
+    display.textContent = result;
+    firstNum = result;
+    secondNum = '';
+});
+
+clear.addEventListener('click', function() {
+    display.textContent = ''; 
+    initOperator = '';
+    firstNum = '';
+    secondNum = ''; 
+    conseqOperator = '';
 });
